6bdd6a1daeae6889a29b65fa4a66edbb3499e216
duhoobo <alecdu@gmail.com>
2015-02-02 19:07:43 +0800

    `plain` credential of basic scheme implemented

--- lib/resty/auth/basic.lua
+++ lib/resty/auth/basic.lua
@@ -1,5 +1,8 @@
 
-local table_concat = table.concat
+local tab_concat = table.concat
+local str_find = string.find
+local str_sub = string.sub
+local str_gsub = string.gsub
 
 
 local _M = {
@@ -14,8 +17,50 @@ local _M = {
 --      user:{PLAIN}cipher
 --      user:{SSHA}base64(SHA1(key+salt)+salt)
 --      user:{SHA}base64(SHA1(key))
-local function parse_credential_line(line)
+local function parse_line(line)
+    return "admin", "plain", "", "admin"
 end
+
+
+local function validate_plain(user, passwd, salt, cipher)
+    print(user, passwd, salt, cipher)
+    return passwd == cipher
+end
+
+local function validate_apr1(user, passwd, salt, cipher)
+end
+
+local function validate_sha(user, passwd, salt, chiper)
+end
+
+local function validate_ssha(user, passwd, salt, cipher)
+end
+
+
+local validators = {
+    plain= validate_plain,
+    apr1= validate_apr1,
+    sha=validate_ssha,
+    ssha= validate_ssha,
+}
+
+local function validate(credentials, user, passwd)
+    if not credentials[user] then
+        return false
+    end
+
+    local cred = credentials[user]
+    return validators[cred.method](user, passwd, cred.salt, cred.cipher)
+end
+
+
+local function challenge(realm)
+    ngx.header.www_authenticate = tab_concat {
+        "Basic realm=\"", realm, "\""}
+
+    return ngx.exit(ngx.HTTP_UNAUTHORIZED)
+end
+
 
 function _M.setup(args)
     if not args.user_file then
@@ -28,16 +73,15 @@ function _M.setup(args)
     end
 
     for line in file:lines() do
-        print(line)
-    --     local username, method, salt, cipher = parse_credential_line(line)
+        local user, method, salt, cipher = parse_line(line)
 
-    --     if username then
-    --         _M.credentials[username] = {
-    --             method= method,
-    --             salt= salt,
-    --             cipher= cipher
-    --         }
-    --     end
+        if user then
+            _M.credentials[user] = {
+                method= method,
+                salt= salt,
+                cipher= cipher
+            }
+        end
     end
 
     file:close()
@@ -45,24 +89,43 @@ function _M.setup(args)
     return true
 end
 
-function _M.challenge(self, realm)
-    ngx.header.www_authenticate = table_concat {
-        "Basic realm=\"", realm, "\""}
-
-    return ngx.exit(ngx.HTTP_UNAUTHORIZED)
-end
 
 function _M.auth(self, realm)
     realm = realm or _M.default_realm
 
+    -- credentials
     local header = ngx.var.http_authorization
     if not header then
-        return self:challenge(realm)
+        return challenge(realm)
     end
 
-    print(header)
+    local prefix = "Basic "
+    if str_sub(header, 1, #prefix) ~= prefix then
+        return challenge(realm)
+    end
 
-    ngx.exit(ngx.OK)
+    -- base64-user-pass
+    local b64_userpass = str_gsub(str_sub(header, #prefix+1), " ", "")
+
+    -- user-pass
+    local userpass = ngx.decode_base64(b64_userpass)
+    if not userpass then
+        return challenge(realm)
+    end
+
+    local colon = str_find(userpass, ":")
+    if not colon then
+        return challenge(realm)
+    end
+
+    local user = str_sub(userpass, 1, colon - 1)
+    local passwd = str_sub(userpass, colon + 1)
+
+    if not validate(self.credentials, user, passwd) then
+        return challenge(realm)
+    end
+
+    return ngx.exit(ngx.OK)
 end
 
 function _M.new()
